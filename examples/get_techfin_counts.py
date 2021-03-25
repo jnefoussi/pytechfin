c47b41b10e41ba7881be2148646da96118e0f3b5
rafarui <rafarui@gmail.com>
2021-03-25 10:18:41 -0300

    apps

--- examples/get_techfin_counts.py
+++ examples/get_techfin_counts.py
@@ -1,10 +1,11 @@
 from pytechfin import Techfin, TOTVSRacAuth, TechfinDataModel
+from pytechfin.enums import EnumApps
 
 
 tf = Techfin()
 dm = TechfinDataModel(tf)
 
 
-r = dm.get_table_record_count( carol_tenant='tenant54b78fbb913d11eab6e00a5864613d5f',)
+r = dm.get_table_record_count( carol_tenant='tenant54b78fbb913d11eab6e00a5864613d5f', techfin_app=EnumApps.CASHFLOW.value)
 
 print(r)
